80bccaa958972bce20b7862e5f3761a11806a553
OpenAVLab <120440723+OpenAVLab@users.noreply.github.com>
2020-02-23 22:01:39 +0000

    Update contacts.md

--- client/components/UserList.test.js
+++ client/components/UserList.test.js
@@ -4,4 +4,11 @@ import UserList from './UserList.jsx'
 import User from './User.jsx'
 
 const fakeUsers = [
-  { id: 1, firstName: "First",  lastName: "User", email: "first.user@example.com"
+  { id: 1, firstName: "First",  lastName: "User", email: "first.user@example.com"},
+  { id: 2, firstName: "Second", lastName: "User", email: "second.user@example.com"},
+  { id: 3, firstName: "Third",  lastName: "User", email: "third.user@example.com"},
+]
+
+describe('UserList Component', () => {
+  it('should render a User component for each user passed in', () => {
+    const userList =
