8e21cc0df9e3683a2251ce376fac5436aba0285b
OpenAVLab <120440723+OpenAVLab@users.noreply.github.com>
2020-03-01 06:17:08 +0000

    updated version and readme

--- client/lib/test-setup.js
+++ client/lib/test-setup.js
@@ -3,4 +3,9 @@ import React from 'react'
 import enz, {shallow as shall} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
-export const ada
+export const adapter = new Adapter()
+enz.configure({adapter})
+
+export const enzyme = enz
+export const shallow = shall
+export const expect = exp
