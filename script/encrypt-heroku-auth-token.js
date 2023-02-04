2bf3532b26ab0488ac7ba8f9b665ee71b21630b3
OpenAVLab <120440723+OpenAVLab@users.noreply.github.com>
2023-02-04 06:38:32 +0000

    Setting version to 0.1.6-SNAPSHOT

--- script/encrypt-heroku-auth-token.js
+++ script/encrypt-heroku-auth-token.js
@@ -5,4 +5,12 @@ const fs = require("fs");
 
 const axios = require("axios");
 const GitUrlParse = require("git-url-parse");
-const simpleGit = require("simple-git")(
+const simpleGit = require("simple-git")();
+const YAML = require("yaml");
+
+/* Specific message contents stored as constants */
+
+const keyComments = require("./keyComments.json");
+
+const idempotenceMessage = `It appears that your token has been encrypted.
+To run this script again, delete the \`before_de
