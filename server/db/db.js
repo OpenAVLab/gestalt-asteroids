7787e1e51a048ed9ca71c9b4ac9490ff3d9fb170
OpenAVLab <120440723+OpenAVLab@users.noreply.github.com>
2023-03-04 09:30:48 +0000

    minor spelling fixes

--- server/db/db.js
+++ server/db/db.js
@@ -1,4 +1,22 @@
 const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
 const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}-test` : pkg.name
-const dbUrl = process.env.DATABASE_
+const dbUrl = process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`
+let config
+if (process.env.DATABASE_URL) {
+  config = {
+    logging: false,
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  }
+} else {
+  config = {
+    logging: false
+  }
+}
+const client = new Seque
